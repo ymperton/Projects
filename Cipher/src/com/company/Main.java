9a09f67e6deca2bd6869e920b0b9d8894d249c14
Yisroel Perton <ymperton@gmail.com>
2017-12-03 19:28:22 -0500

    Started Poly part

--- Cipher/src/com/company/Main.java
+++ Cipher/src/com/company/Main.java
@@ -6,54 +6,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        CaesarianCipher cc = new CaesarianCipher();
-//        cc.enterCipherTextAndClean("XtwnlmystbNrtsymjxzgbfdgfhpytqfsijwxfkyjwrdifyjbjrjyfyjcfhyqdfsibjbfqpjiytOjwzxfqjrhfkjbm" +
-//                "nhmxmjsjajwbfxymjwjgjktwjxmjtwijwjifbfyjwfsiNltyfXsfuuqjBjknwxyxutpjfgtzyNxwfjqfsiymjJqqrfsxNitsypst" +
-//                "bnkxmjbfxitnslrtxytkymjyfqpnslfsixtNbtzqijsyjwbmfyNbfsyjiytxfdNrltnslytmfajytlttsfstymjwYmjwjbjwjufw" +
-//                "yxnsynrjymfyNbfxymnspnslytrdxjqkymfybmtfNrknsfqqditnslfifyjNitsypstbmtbqtslmfxgjjsgznqinslzuytymnxgz" +
-//                "yNbfxymjwjBjxutpjfgtzymjwkfymjwxxmzqNymnspymfyNbfxymjtsjrtxyqdfxpnslymjvzjxyntsxgzyrfdgjxmjbfxxmdNit" +
-//                "sypstbnkymnxbfxmjwknwxyifyjfxbjqqNsymjrniiqjbjbjsyytljykttixmjltyfunjhjtkxfqrtsfsiNltyxzxmnNbnxmxmjl" +
-//                "tyrtwjgzyymfyxbmfyxmjbfsyjiNmtujnyitjxsywjkqjhyuttwqdtsrjktwNbfxfgqjytxujsirtwjBjltynsythfruzxYjfsjh" +
-//                "pxyjwsTpfdfwtzsibmnhmbfxtajwybtmtzwxtkyfpnslbmnhmbjsygdkfxyymjdxyfwyjiythqtxjxtbjqjkyfsibfqpjigfhpyt" +
-//                "tzwrjjynslutnsybmnhmbfxtzyxnijtkmjwitwrnytwdstbxmjinisybfsyrjytyfpjmjwfqqymjbfdzuytymjittwNlzjxxktwt" +
-//                "gantzxwjfxtsxgzyxmjinisybfsyytltnswnlmyfbfdxtbjinifqttufwtzsifsiymjsbjinifstymjwqttufwtzsiynqqnybfxf" +
-//                "siymjsbjxuqnyzuRdfsxbjwnxymfydjxNbtzqibfsyytxjjmjwflfnsGzyymjkjjqnslbfxhwfedbmjsNxfbmjwljyytymjhtwsj" +
-//                "wfyymjgjlnssnsltkymjsnlmyfsiNbfxqnpjytrdxjqkymfybtfmymnxnxnyymnxnxmjwNwjhtlsnejmjwkwtrymjunhyzwjNits" +
-//                "ypstbnkxmjlttlqjirjfsipsjbbmfyNqttpjiqnpjNbtzqixfduwtgfgqddjxktwtkhtzwxjdtzLttlqjymjujwxtsstygqnsiqd" +
-//                "ktqqtbnslfwjxzrjXtrdufwjsyxbfsyrjythfqqymjrbmjsNljygfhpNxynqqitsypstbjajwdymnslfgtzymjwbmnhmrfpjxxhj" +
-//                "sjFsiymjwjfwjhjwyfnsymnslxbmnhmNrltnslytmfajytktqqtbzutsnkymjwjbnqqgjfsjcyynrjqnpjymjinxxjhynsltkfkw" +
-//                "tlbmnhmxmjnxltnslytitns2bjjpxgzybnqqrjjynyytrtwwtbUzwnrbnqqgjnsgjybjjsfsixmjbtzqimfajknlzwjitzymjwht" +
-//                "xyzrjNyxjjrjiqnpjnybfxmfwiktwmjwytrfpjijhnxntsxbmjsnyhfrjytbmnhmbfdytbjjpbmnhmnxsyfgnlijfqNpstbxmjmf" +
-//                "xrtwjytmjwymjsymjxzwkfhjgjhfzxjNiniymjwjxjfwhmtsmjwNrjsyntsjiTMJQGjnxJewffsiNbfxhfwjkzqytutnsytzyymfy" +
-//                "NitsyijknsjnsinanizfqxgdymjnwinxfgnqnydxtNlzjxxymfyxfuqzxfsiNbfxsyqdnslytmjwymfyxbmfyNfrNozxymfiytrfp" +
-//                "jxzwjymfyNxfninyhtwwjhyqdFsiNinisyrnsibfqpnslbnymmjwNbtzqimfajwfymjwhtsynszjibfqpnslqfyjwozxyymfyNsjj" +
-//                "iytljygfhpytVzjjsxFqxtfstymjwymnslymfyNqnpjibfxymfyxmjitjxsylttzyfsijcuqtwjymjhnydstynsfgfibfdgzyymfy" +
-//                "xmjnxsytsjythtsxyfsyqdsjjiytmfsltzybnymkwnjsixfsilttzyfsijfyNrtsqdfkwfniymfyxmjbfxsyfgqjytljyflttiwjfi" +
-//                "tsrjtwrfdgjxmjinifsiitjxsyymnspymfyNrktwmjwfsinkxmjijhnijiymfyymjsTPnybfxsyrjfsyytgjgzyNhtzqixjjnybtwp" +
-//                "nsltzygjybjjszxFyknwxynybfxfgnybjnwigjhfzxjxmjxutpjajwdqtbfsinybfxmfwiytmjfwmjwfyknwxygzyymfybjsyfbfdKtw" +
-//                "xtrjwjfxtsNljyymfyxmjnxfsnsywtajwyXmjxajwdrzhmnsytQnrzijnPtijxmfxxmjbfxxfdnslymfyxmjkjqygfiymfyxmjbtsymf" +
-//                "ajfqjfwsnslhqfxxnstsjtkymjsjcyxjrjxyjwxfsixmjbfxrjsyntsnslmtbnybnqqgjhtsxnijwjiqxmrf");
-
-//        PlainText pt = cc.decipher();
-//        System.out.println(pt);
-long start = System.currentTimeMillis();
-        PolyalphabeticCipher pc = new PolyalphabeticCipher();
-        pc.enterCipherTextAndClean("CSPEUYWXSUEAFQDLCOISZKCZW QBWYPYJRVUCEDPSIPIHYPSNHW IRWHVAKGRHMRQNACSOCNOHRKX " +
-                "VUEWYHSDFKJCSVZFRWFABVYOV UWGKKOVCXSWRBIQDSFUNIPARR ZKXCNOEGSKMPOJQKTNHSNHPMP OHJSYOCWPFXDMQNOVOKRBPVVH " +
-                "VPKWBJLNSLPYERGMDOVVZKWBK WEJWSQPCWWRIRWZBLXKYJRJRS AMQZUHXXCNKYDDMUWBKHNXMOO PLWKMEBXWYLYRSKRQSMJOERDL " +
-                "CNHYHBIUAFVSKVROWEWSQCPVR WSAYOHYLXOGJUKRWCQAZWWRER SVFDSQDEBROVCBKWEJKHYPSZG YRRGBFZRSUHCEJREQXSVQLYGH " +
-                "RZQQYNPCKKSWZQHZZKWRDSIHG IQLCBHKFMQHYHBJYPVVUCWFQZ ZWRMLGHYDDMUWGKKOSLAAFVDP WWGBLXKRDSHXOWRECEVLYRIOP " +
-                "EOWFAKRVCLWERFQDOLKKZIDLG OKRVRIPBWIVDHYPSRVGIJHWEW RIKERUOOACSSEWDSEAHWRYHQD SXRDENESTHYJQWZDRXELZWXRD " +
-                "WSOVZLGMQDGYHQSRICIHLYRPV RWCAFWHJKOAYJHVGSLMLSZWNS COBKUOJJAQKSYSPHMFQWIDKFZ ZKWYXZVWYWNABUPYVCSSXRDML " +
-                "PCTDWTSOHVDXIAGGKHBRMGOPD BSSJRNKSGFSOJRFIPPKFKYYPO CWWKOGJUNKSGFSSEWLCDWGKWR IWOHRUDIBPCTOYWCOCNHVIDPO");
-
-        PlainText pt = pc.decipher();
-        System.out.println(pt.toString());
-
-        long end = System.currentTimeMillis();
-        long time = (end - start) / 1000;
-        System.out.println("time: " + time);
-        System.out.println("Done");
-
     }
 }
